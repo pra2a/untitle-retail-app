7c882528cfc2192e8d83a95a54ce1a423d75f622
Luis Prada <pradam.luisadej@gmail.com>
2019-04-01 14:57:58 -0400

    Updates

---
+++ precache-manifest.06119fa76c0b3406d8a4fe3978c6832b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/untitle-retail-app/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "210459358614e1062d60",
+    "url": "/untitle-retail-app/static/css/main.d077fd65.chunk.css"
+  },
+  {
+    "revision": "3baaffc27a6deb916a88",
+    "url": "/untitle-retail-app/static/js/runtime~main.3baaffc2.js"
+  },
+  {
+    "revision": "61579e5019cd8e6c91139fbfc4c1484d",
+    "url": "/untitle-retail-app/static/media/190.61579e50.jpg"
+  },
+  {
+    "revision": "32e31dc46adc4e24e41e",
+    "url": "/untitle-retail-app/static/js/2.32e31dc4.chunk.js"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/untitle-retail-app/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "210459358614e1062d60",
+    "url": "/untitle-retail-app/static/js/main.21045935.chunk.js"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/untitle-retail-app/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/untitle-retail-app/static/media/sidebar-4.fc9cb053.jpg"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/untitle-retail-app/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "32e31dc46adc4e24e41e",
+    "url": "/untitle-retail-app/static/css/2.fa27a847.chunk.css"
+  },
+  {
+    "revision": "6f927c9468a753417a3ceb2ca3e75698",
+    "url": "/untitle-retail-app/index.html"
+  }
+];
